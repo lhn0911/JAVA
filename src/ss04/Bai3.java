39a9b3c04a32526d2e904362e3be7153446c3c58
lhn0911 <154567669+lhn0911@users.noreply.github.com>
2025-03-05 09:45:31 +0700

    Add files via upload

--- src/ss04/Bai3.java
+++ src/ss04/Bai3.java
@@ -6,13 +6,17 @@ public class Bai3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        if (n < 1) {
-            System.out.println("N khong hop le");
+
+        if (n < 0) {
+            System.out.println("Số nhập vào không hợp lệ");
+            return;
+        } else if (n > 10) {
+            System.out.println("Số nhập vào phải trong khoảng từ 1 - 10");
             return;
         }
 
         for (int i = 1; i <= 10; i++) {
-            System.out.println(n + "x" + i + "=" + (n * i));
+            System.out.println(n + " x " + i + " = " + (n * i));
         }
     }
 }
