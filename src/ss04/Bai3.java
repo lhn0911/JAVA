3a11845060b01cd82a2cc4d6b4769c5a82dd0bbc
lhn0911 <154567669+lhn0911@users.noreply.github.com>
2025-03-05 08:19:58 +0700

    Add files via upload

--- src/ss04/Bai3.java
+++ src/ss04/Bai3.java
@@ -6,14 +6,13 @@ public class Bai3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        if(n<1){
+        if (n < 1) {
             System.out.println("N khong hop le");
             return;
         }
-        else{
-            for(int i=1;i<=10;i++){
-                System.out.println(n+"x"+i+"="+(n*i));
-            }
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + "x" + i + "=" + (n * i));
         }
     }
 }
